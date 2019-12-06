8cf52effb06d4014d9bb4e057fbda520d04c785e
wesley gutierrez <57546607+wesleygutierrez@users.noreply.github.com>
2019-12-05 21:55:46 -0300

    exe8

---
+++ exercicio 8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	float valor, resultado;
+	
+	printf("coloque um valor:");
+	scanf("%f", &valor);
+	
+	resultado=valor/4;
+	
+	printf("resultado:%f", resultado);	
+}
